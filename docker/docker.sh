2a6994b2151526b48e1e5a4f4a077facefb06cd9
madmazoku <madmazoku@gmail.com>
2018-04-13 19:50:36 +0300

    update docker

--- docker/docker.sh
+++ docker/docker.sh
@@ -1,7 +1,7 @@
 echo ========= Build docker image
-docker build -t otus.lessons.22.01 .
+docker build -t otus.lessons.26.01 .
 # echo ========= Execute bulk_server
-# docker run --rm -i otus.lessons.22.01 
-# ( ./bulk_server 9999 3 ) & (sleep 1 ; seq 1 10 | nc localhost 9999 ; pkill -INT bulk_server)
+# docker run --rm -i otus.lessons.26.01 
+# ( ./join_server 9999 ) & (perl -e 'print "INSERT A 1 A\nINSERT B 1 B\nDUMP A\nDUMP B\nINTERSECTION\n";' | nc localhost 9999 ; pkill -INT bulk_server)
 echo ========= Remove docker image
-docker rmi otus.lessons.22.01
+docker rmi otus.lessons.26.01
